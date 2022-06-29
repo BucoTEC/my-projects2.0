b5a1e58020939234e4c6d3b5d4bc4b42fb2ad9be
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-30 00:40:48 +0200

    refomr fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,38 +1,36 @@
-import { Injectable } from '@nestjs/common'
-import { Model } from 'mongoose'
-import { InjectModel } from '@nestjs/mongoose'
-import { User, UserDocument } from 'src/schemas/user.schema'
-import { hashPassword } from 'src/utils/hashPassword'
+import { Injectable } from "@nestjs/common";
+import { Model } from "mongoose";
+import { InjectModel } from "@nestjs/mongoose";
+import { User, UserDocument } from "src/schemas/user.schema";
+import { hashPassword } from "src/utils/hashPassword";
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectModel(User.name) private userModel: Model<UserDocument>,
-    ) {}
-    // TODO add login dto && logic
-    login() {
-        return 'login service'
-    }
-    //  TODO add register dto
-    async register(body) {
-        const { username, password, email } = body
-        const existingUser = await this.userModel.find({ email })
-        if (existingUser) 'user already exists'
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  // TODO add login dto && logic
+  login() {
+    return "login service";
+  }
+  //  TODO add register dto
+  async register(body) {
+    const { username, password, email } = body;
+    const existingUser = await this.userModel.find({ email });
+    if (existingUser) "user already exists";
 
-        const hashedPassword = await hashPassword(password)
+    const hashedPassword = await hashPassword(password);
 
-        const newUser = new this.userModel({
-            username,
-            email,
-            password: hashedPassword,
-        })
+    const newUser = new this.userModel({
+      username,
+      email,
+      password: hashedPassword,
+    });
 
-        await newUser.save()
-        return {
-            msg: 'successfuly registerd',
-            userData: {
-                id: newUser.id,
-                email: newUser.email,
-            },
-        }
-    }
+    await newUser.save();
+    return {
+      msg: "successfuly registerd",
+      userData: {
+        id: newUser.id,
+        email: newUser.email,
+      },
+    };
+  }
 }
--- src/utils/hashPassword.ts
+++ src/utils/hashPassword.ts
@@ -1,11 +1,9 @@
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from "bcrypt";
 
-type hashReturn = Promise<string> | void
+type hashReturn = Promise<string> | void;
 
-export const hashPassword = async (
-    plainPassword: string,
-): Promise<hashReturn> => {
-    const hashPassword = await bcrypt.hash(plainPassword, 12)
+export const hashPassword = async (plainPassword: string): Promise<hashReturn> => {
+  const hashPassword = await bcrypt.hash(plainPassword, 12);
 
-    return hashPassword
-}
+  return hashPassword;
+};
