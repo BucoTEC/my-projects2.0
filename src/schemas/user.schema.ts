4197f4dbbfcf75b70aa2cafd72fa4733fa1c09e1
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-29 23:38:18 +0200

    setup user schema

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({
+    required: true,
+    min: 3,
+    max: 30,
+  })
+  username: string;
+
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    required: true,
+    min: 4,
+  })
+  password: string;
+}
+
+export const ProjectSchema = SchemaFactory.createForClass(User);
