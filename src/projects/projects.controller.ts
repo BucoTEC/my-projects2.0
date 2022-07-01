3121ee92e7451923b10092fc4f131b369a21f81b
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-01 12:33:10 +0200

    configur basic auth guard

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { AuthGuard } from 'src/guards/auth.guard';
 @Controller('projects')
+@UseGuards(AuthGuard)
 export class ProjectsController {
   constructor(private configService: ConfigService) {}
   @Get()
-  projects() {
+  projects(@Request() req) {
+    console.log(req.message);
+
     const test = this.configService.get<string>('TEST');
     return { msg: 'hellp', test };
   }
