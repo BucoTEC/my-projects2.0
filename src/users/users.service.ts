cf4cc80e50254b32b405bcc0b172a9bb1e2f6a1c
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-02 17:39:23 +0200

    add find all users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/schemas/user.schema';
 
 @Injectable()
 export class UsersService {
-  allUsers() {
-    return 'all users serivce';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  async allUsers() {
+    const allUsers = await await this.userModel
+      .find({})
+      .select({ username: 1, email: 1 });
+    return { msg: 'all users', listAllUsers: allUsers };
   }
 
   singleUser(id: string) {
