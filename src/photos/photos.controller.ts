3ab3d5551a6a20c84ee1aa559da26163b4670b97
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-02 15:12:05 +0200

    setup basic clodinary service

--- src/photos/photos.controller.ts
+++ src/photos/photos.controller.ts
@@ -1,18 +1,22 @@
 import {
+  BadRequestException,
   Controller,
   Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Controller('photos')
 export class PhotosController {
+  constructor(private cloudinary: CloudinaryService) {}
+
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  uploadSingle(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
-
-    return 'upload single route';
+  async uploadSingle(@UploadedFile() file: Express.Multer.File) {
+    return await this.cloudinary.uploadImage(file).catch(() => {
+      throw new BadRequestException('Invalid file type.');
+    });
   }
 }
--- src/photos/photos.module.ts
+++ src/photos/photos.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 import { PhotosController } from './photos.controller';
 
 @Module({
+  imports: [CloudinaryModule],
   controllers: [PhotosController],
 })
 export class PhotosModule {}
